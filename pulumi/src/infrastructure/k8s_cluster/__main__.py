8979cd571b019550a2e72506c8ca6e5252ceb36e
tapanihook <tapani.hook@netum.fi>
2022-07-21 08:41:12 +0300

    refactor: add option to store node capacity in pulumi config

--- pulumi/src/infrastructure/k8s_cluster/__main__.py
+++ pulumi/src/infrastructure/k8s_cluster/__main__.py
@@ -10,7 +10,6 @@ import pulumi
 from pulumi_eks import Cluster
 
 
-cluster_config = Config("cluster_config")
 network_config = Config("network_config")
 network_stack = pulumi.StackReference(
     "shared-network",
@@ -19,20 +18,31 @@ k8s_cluster_vpc_id = network_stack.outputs["vpc_id"]
 my_private_subnet_ids = network_stack.outputs["private_subnet_ids"]
 pulumi.log.debug(f"Subnet ids: {my_private_subnet_ids}")
 pulumi.log.debug(f"VPC id: {k8s_cluster_vpc_id}")
+
+cluster_config = Config("cluster_config")
 base_name = f"{cluster_config.require('environment')}-k8s-cluster"
+desired_node_capacity = cluster_config.get("desired_node_capacity")
+if not desired_node_capacity:
+    desired_node_capacity = 2
+min_node_capacity = cluster_config.get("min_node_capacity")
+if not min_node_capacity:
+    min_node_capacity = 1
+max_node_capacity = cluster_config.get("max_node_capacity")
+if not max_node_capacity:
+    max_node_capacity = 3
 
 # TODO: Add tags
 my_k8s_cluster = Cluster(
     base_name,
     create_oidc_provider=True,
-    desired_capacity=2,
+    desired_capacity=desired_node_capacity,
     enabled_cluster_log_types=[
         "api", "audit", "authenticator", "controllerManager", "scheduler"],
     encrypt_root_block_device=True,
-    instance_type="m5.large",
+    instance_type="t3.large",
     name=base_name,
-    max_size=3,
-    min_size=2,
+    max_size=max_node_capacity,
+    min_size=min_node_capacity,
     node_root_volume_encrypted=True,
     node_root_volume_size=100,
     node_ami_id=cluster_config.require("node_ami_id"),
